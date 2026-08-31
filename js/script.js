c3c9de03b088a66296bcc98e5b0c7e4b2a9b05e7
Aida-9 <aigarsan19994@gmail.com>
2026-08-31 12:09:00 +0200

    crear un script que permita facer o menu nav responsive para movil

---
+++ js/script.js
@@ -0,0 +1,10 @@
+<script>
+
+const boton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+boton.addEventListener("click", () => {
+    menu.classList.toggle("activo");
+});
+
+</script>
