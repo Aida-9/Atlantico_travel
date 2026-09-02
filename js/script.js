4a51a456dcca44c30bbcf59ec7105f9bb6f24bc5
Aida-9 <aigarsan19994@gmail.com>
2026-09-02 10:34:32 +0200

    preparar as seccions para cambiarlle  o idioma

--- js/script.js
+++ js/script.js
@@ -11,3 +11,42 @@ if (botonMenu && menu) {
     });
 
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const selectorIdioma = document.querySelector("#idioma");
+
+    const traduccions = {
+        gl: {
+            "Inicio": "Inicio",
+            "Sobre nós": "Sobre nós",
+            "Destinos": "Destinos",
+            "Experiencias": "Experiencias",
+            "Ofertas": "Ofertas",
+            "Contacto": "Contacto",
+            "Solicita información": "Solicita información",
+            "Preparados para a aventura?": "Preparados para a aventura?"
+        },
+      es: {
+            "Inicio": "Inicio",
+            "Sobre nós": "Sobre nosotros",
+            "Destinos": "Destinos",
+            "Experiencias": "Experiencias",
+            "Ofertas": "Ofertas",
+            "Contacto": "Contacto",
+            "Solicita información": "Solicita información",
+            "Preparados para a aventura?": "¿Preparados para la aventura?"
+        },
+    en: {
+            "Inicio": "Home",
+            "Sobre nós": "About us",
+            "Destinos": "Destinations",
+            "Experiencias": "Experiences",
+            "Ofertas": "Offers",
+            "Contacto": "Contact",
+            "Solicita información": "Request information",
+            "Preparados para a aventura?": "Ready for the adventure?"
+        }
+
+    };
+    
